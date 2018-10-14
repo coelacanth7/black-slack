abf3eb839ebeeba3f7c6c5f4001e0186002427df
Coelacanth7 <galford151@gmail.com>
2018-10-13 19:31:09 -0700

    injectedJS

---
+++ injectedJS.js
@@ -0,0 +1,16 @@
+// https://gist.github.com/a7madgamal/c2ce04dde8520f426005e5ed28da8608
+
+// This is the JS file to inject into the Slack Applications folder
+
+// The css file to apply
+const cssFile = "black"
+
+document.addEventListener("DOMContentLoaded", function() {
+  const fs = require("fs");
+  fs.readFile(`/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/${cssFile}.css`, "utf8", (err, css) => {
+    if (err) throw err;
+    $("<style></style>")
+      .appendTo("head")
+      .html(css);
+  });
+});
