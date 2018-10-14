c0f72b8e33343050b1430316267c60373728e808
Coelacanth7 <galford151@gmail.com>
2018-10-13 22:45:36 -0700

    refactor constants

---
+++ js/constants.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+const constants = {};
+
+// File paths
+constants.slackFile =
+	"/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js";
+constants.cssFilePath =
+	"/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/black.css";
+constants.injectedJSFilePath = path.resolve(__dirname, "./jstoinject.js");
+constants.injectedCssFilePath = path.resolve(__dirname, "../css/black.css");
+
+// injected Data
+constants.injectedJSFile = fs.readFileSync(
+	constants.injectedJSFilePath,
+	"utf8"
+);
+constants.injectedCssFile = fs.readFileSync(
+	constants.injectedCssFilePath,
+	"utf8"
+);
+
+// document elements
+constants.cssSelecet = document.getElementById("css-file-select");
+constants.injectButton = document.getElementById("inject");
+
+module.exports = constants;
--- js/injector.js
+++ js/injector.js
@@ -1,27 +1,13 @@
 const fs = require("fs");
-const path = require("path");
 
-const logger = require('./logger')
-
-// File paths
-const slackFile =
-	"/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js";
-const cssFilePath =
-	"/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/black.css";
-const injectedJSFilePath = path.resolve(__dirname, "./injector.js");
-const injectedCssFilePath = path.resolve(__dirname, "../css/black.css");
-
-// injected Data
-const injectedJSFile = fs.readFileSync(injectedJSFilePath, "utf8");
-const injectedCssFile = fs.readFileSync(injectedCssFilePath, "utf8");
-
-// document elements
-const cssSelecet = document.getElementById("css-file-select");
-const injectButton = document.getElementById("inject");
+const logger = require("./logger");
+const constants = require("./constants");
 
 // listeners
-cssSelecet.addEventListener("change", e => logger.log(e.target.value));
-injectButton.addEventListener("click", handleFlow);
+constants.cssSelecet.addEventListener("change", e =>
+	logger.log(e.target.value)
+);
+constants.injectButton.addEventListener("click", handleFlow);
 
 // handle the flow
 function handleFlow() {
@@ -49,15 +35,15 @@ function checkIfJsHasInjected() {
 	// try
 	try {
 		// read the file buffer
-		ssbInteropBuffer = fs.readFileSync(slackFile);
-		injectedJSFileBuffer = fs.readFileSync(injectedJSFilePath);
+		ssbInteropBuffer = fs.readFileSync(constants.slackFile);
+		injectedJSFileBuffer = fs.readFileSync(constants.injectedJSFilePath);
 	} catch (e) {
 		// log errors
 		logger.error(e);
 	} finally {
 		// check if slack application file has injectedJS
-    includesTheJS  = ssbInteropBuffer.indexOf(injectedJSFileBuffer) > -1
-		logger.log("your js file exists = ", includesTheJS);
+		includesTheJS = ssbInteropBuffer.indexOf(injectedJSFileBuffer) > -1;
+		logger.log(`your js file exists = ${includesTheJS}`);
 
 		// return bool
 		return includesTheJS;
@@ -70,11 +56,11 @@ function checkIfCssHasInjected() {
 	// try
 	try {
 		// does the css exist yet?
-		cssExists = fs.existsSync(cssFilePath);
+		cssExists = fs.existsSync(constants.cssFilePath);
 	} catch (e) {
 		logger.error(e);
 	} finally {
-		logger.log("your css file exists = ", cssExists);
+		logger.log(`your css file exists = ${cssExists}`);
 		return cssExists;
 	}
 }
@@ -83,7 +69,7 @@ function thenInjectCSS() {
 	// try
 	try {
 		// append injectedJS to file
-		fs.writeFileSync(cssFilePath, injectedCssFile);
+		fs.writeFileSync(constants.cssFilePath, constants.injectedCssFile);
 		logger.log(`Appending CSS hack to path ${cssFilePath}`);
 	} catch (e) {
 		// log errors
@@ -95,7 +81,7 @@ function thenInjectJS() {
 	// try
 	try {
 		// append injectedJS to file
-		fs.appendFileSync(slackFile, injectedJSFile);
+		fs.appendFileSync(constants.slackFile, constants.injectedJSFile);
 		logger.log(`Appending JS hack to path ${slackFile}`);
 	} catch (e) {
 		// log errors
